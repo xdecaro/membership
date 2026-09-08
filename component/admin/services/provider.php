0e7b410234dcf7372b64876a2c36262e6b75ef0b
xdecaro <yvv4jfzkbb@privaterelay.appleid.com>
2026-09-08 02:25:59 +0200

    Register optional Membership Core integration service

--- component/admin/services/provider.php
+++ component/admin/services/provider.php
@@ -10,15 +10,23 @@ use Joomla\CMS\Extension\Service\Provider\MVCFactory;
 use Joomla\CMS\MVC\Factory\MVCFactoryInterface;
 use Joomla\DI\Container;
 use Joomla\DI\ServiceProviderInterface;
+use Xdecaro\Component\Decaromembership\Administrator\Service\CoreIntegrationService;
 
 return new class () implements ServiceProviderInterface {
     public function register(Container $container): void
     {
         $container->registerServiceProvider(new ComponentDispatcherFactory('\\Xdecaro\\Component\\Decaromembership'));
         $container->registerServiceProvider(new MVCFactory('\\Xdecaro\\Component\\Decaromembership'));
+
+        $container->share(
+            CoreIntegrationService::class,
+            static fn (Container $container): CoreIntegrationService => new CoreIntegrationService()
+        );
+
         $container->set(ComponentInterface::class, static function (Container $container): ComponentInterface {
             $component = new MVCComponent($container->get(ComponentDispatcherFactoryInterface::class));
             $component->setMVCFactory($container->get(MVCFactoryInterface::class));
+
             return $component;
         });
     }
