339a6a444a776ebc66e47ec5d82d9bf6384944f5
xdecaro <yvv4jfzkbb@privaterelay.appleid.com>
2026-09-15 23:40:12 +0200

    fix: render Membership CSS and JS directly in record template

--- component/admin/tmpl/record/default.php
+++ component/admin/tmpl/record/default.php
@@ -3,17 +3,10 @@ defined('_JEXEC') or die;
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\Router\Route;
-$wa=$this->getDocument()->getWebAssetManager();
-if(!$wa->assetExists('style','com_decaromembership.admin')){
-    $wa->registerAndUseStyle('com_decaromembership.admin','com_decaromembership/css/admin.css',['version'=>'auto']);
-}else{
-    $wa->useStyle('com_decaromembership.admin');
-}
-if(!$wa->assetExists('script','com_decaromembership.admin')){
-    $wa->registerAndUseScript('com_decaromembership.admin','com_decaromembership/js/admin.js',['version'=>'auto'],['defer'=>true],['core']);
-}else{
-    $wa->useScript('com_decaromembership.admin');
-}
+use Joomla\CMS\Uri\Uri;
+$membershipAssetBase=rtrim(Uri::root(true),'/').'/media/com_decaromembership';
+$membershipCssVersion=@filemtime(JPATH_ROOT.'/media/com_decaromembership/css/admin.css')?:'1.5.0';
+$membershipJsVersion=@filemtime(JPATH_ROOT.'/media/com_decaromembership/js/admin.js')?:'1.5.0';
 $esc=fn($v)=>htmlspecialchars((string)$v,ENT_QUOTES,'UTF-8');
 $peopleOwnedFields=['person_uuid','first_name','last_name','birth_date','birth_place','tax_code','address','city','province','postal_code','country','email','phone','user_id'];
 $renderField=function(string $name,array $field,mixed $value) use($esc){
@@ -33,6 +26,7 @@ $renderField=function(string $name,array $field,mixed $value) use($esc){
 $linkedUuid=strtolower(trim((string)($this->item->person_uuid??'')));
 $isMember=$this->entity==='members';
 ?>
+<link rel="stylesheet" href="<?= $esc($membershipAssetBase.'/css/admin.css?v='.$membershipCssVersion) ?>">
 <form action="<?= Route::_('index.php?option=com_decaromembership&entity='.$this->entity.'&id='.(int)($this->item->id??0)) ?>" method="post" name="adminForm" id="adminForm" class="dm-page">
 <?php if($isMember): ?>
   <section class="dm-card dm-person-card">
@@ -83,3 +77,4 @@ $isMember=$this->entity==='members';
 <?php endforeach; ?>
 </div>
 <input type="hidden" name="id" value="<?= (int)($this->item->id??0) ?>"><input type="hidden" name="entity" value="<?= $esc($this->entity) ?>"><input type="hidden" name="task" value=""><?= HTMLHelper::_('form.token') ?></form>
+<script src="<?= $esc($membershipAssetBase.'/js/admin.js?v='.$membershipJsVersion) ?>"></script>
