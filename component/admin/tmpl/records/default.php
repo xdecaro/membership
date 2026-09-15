56296d0d2b33d3f1b7932e59dfad5acbfa9b6395
xdecaro <yvv4jfzkbb@privaterelay.appleid.com>
2026-09-15 23:27:58 +0200

    fix: load Membership assets from list template

--- component/admin/tmpl/records/default.php
+++ component/admin/tmpl/records/default.php
@@ -3,6 +3,17 @@ defined('_JEXEC') or die;
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\Router\Route;
+$wa=$this->getDocument()->getWebAssetManager();
+if(!$wa->assetExists('style','com_decaromembership.admin')){
+    $wa->registerAndUseStyle('com_decaromembership.admin','com_decaromembership/css/admin.css',['version'=>'auto']);
+}else{
+    $wa->useStyle('com_decaromembership.admin');
+}
+if(!$wa->assetExists('script','com_decaromembership.admin')){
+    $wa->registerAndUseScript('com_decaromembership.admin','com_decaromembership/js/admin.js',['version'=>'auto'],['defer'=>true],['core']);
+}else{
+    $wa->useScript('com_decaromembership.admin');
+}
 $app=\Joomla\CMS\Factory::getApplication();
 $search=$app->input->getString('filter_search','');
 $peopleLink=$this->entity==='members'?$app->input->getCmd('people_link','all'):'all';
