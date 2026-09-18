a773cc8acdbd3673935f5905160bbd16e509b629
xdecaro <yvv4jfzkbb@privaterelay.appleid.com>
2026-09-18 12:24:16 +0200

    fix: use centralized assets on Membership dashboard

--- component/admin/src/View/Dashboard/HtmlView.php
+++ component/admin/src/View/Dashboard/HtmlView.php
@@ -5,25 +5,14 @@ use Joomla\CMS\Factory;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\MVC\View\HtmlView as BaseHtmlView;
 use Joomla\CMS\Toolbar\ToolbarHelper;
+use Xdecaro\Component\Decaromembership\Administrator\Service\AdminAssetService;
 final class HtmlView extends BaseHtmlView
 {
     public array $data = [];
     public function display($tpl = null): void
     {
         $this->data = $this->get('DashboardData');
-        $wa = $this->getDocument()->getWebAssetManager();
-        $wa->registerAndUseStyle(
-            'com_decaromembership.admin',
-            'com_decaromembership/css/admin.css',
-            ['version' => 'auto']
-        );
-        $wa->registerAndUseScript(
-            'com_decaromembership.admin',
-            'com_decaromembership/js/admin.js',
-            ['version' => 'auto'],
-            ['defer' => true],
-            ['core']
-        );
+        AdminAssetService::useAssets($this->getDocument());
         ToolbarHelper::title(Text::_('COM_DECAROMEMBERSHIP_DASHBOARD'), 'users');
         if (Factory::getApplication()->getIdentity()->authorise('core.admin', 'com_decaromembership')) ToolbarHelper::preferences('com_decaromembership');
         parent::display($tpl);
