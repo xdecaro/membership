56ea0dac4f4af87011dfe970ae1611db63984e51
xdecaro <yvv4jfzkbb@privaterelay.appleid.com>
2026-09-18 12:24:36 +0200

    fix: remove duplicate dashboard asset tags

--- component/admin/tmpl/dashboard/default.php
+++ component/admin/tmpl/dashboard/default.php
@@ -2,9 +2,6 @@
 defined('_JEXEC') or die;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\Router\Route;
-use Joomla\CMS\Uri\Uri;
-$membershipAssetBase=rtrim(Uri::root(true),'/').'/media/com_decaromembership';
-$membershipCssVersion=@filemtime(JPATH_ROOT.'/media/com_decaromembership/css/admin.css')?:'1.5.0';
 $cards=['members'=>'COM_DECAROMEMBERSHIP_MEMBERS','cases'=>'COM_DECAROMEMBERSHIP_CASES','renewals'=>'COM_DECAROMEMBERSHIP_RENEWALS','cards'=>'COM_DECAROMEMBERSHIP_CARDS','dues'=>'COM_DECAROMEMBERSHIP_DUES','payments'=>'COM_DECAROMEMBERSHIP_PAYMENTS','transfers'=>'COM_DECAROMEMBERSHIP_TRANSFERS','documents'=>'COM_DECAROMEMBERSHIP_DOCUMENTS'];
 ?>
 <link rel="stylesheet" href="<?= htmlspecialchars($membershipAssetBase.'/css/admin.css?v='.$membershipCssVersion,ENT_QUOTES,'UTF-8') ?>">
