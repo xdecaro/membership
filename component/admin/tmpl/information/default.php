7a852926dc8632f96cfc78a056714d6a07786c19
xdecaro <yvv4jfzkbb@privaterelay.appleid.com>
2026-09-18 12:29:28 +0200

    fix: remove remaining manual Membership asset tags

--- component/admin/tmpl/information/default.php
+++ component/admin/tmpl/information/default.php
@@ -4,7 +4,7 @@ use Joomla\CMS\Language\Text;
 $escape = static fn (mixed $value): string => htmlspecialchars((string) $value, ENT_QUOTES, 'UTF-8');
 $dependencyBadge = static fn(array $dependency): string => ($dependency['available'] ?? false) ? 'is-ok' : 'is-bad';
 ?>
-<link rel="stylesheet" href="<?= $escape($membershipAssetBase.'/css/admin.css?v='.$membershipCssVersion) ?>">
+
 <div class="dm-page dm-info">
     <div class="dm-grid dm-grid-2">
         <section class="dm-card"><h2><?= Text::_('COM_DECAROMEMBERSHIP_PRODUCT') ?></h2><dl><dt><?= Text::_('COM_DECAROMEMBERSHIP_NAME') ?></dt><dd>Membership</dd><dt><?= Text::_('COM_DECAROMEMBERSHIP_VERSION') ?></dt><dd><?= $escape($this->info['extensions'][0]['version'] ?? '1.6.1') ?></dd><dt><?= Text::_('COM_DECAROMEMBERSHIP_COMPONENT') ?></dt><dd>com_decaromembership</dd><dt><?= Text::_('COM_DECAROMEMBERSHIP_PACKAGE') ?></dt><dd>pkg_decaromembership</dd></dl></section>
