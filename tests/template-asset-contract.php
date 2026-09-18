a43f763b13e1c89559e1ebdd0173d485733cf042
xdecaro <yvv4jfzkbb@privaterelay.appleid.com>
2026-09-18 13:57:29 +0200

    test: require Membership direct CSS and JS paths

--- tests/template-asset-contract.php
+++ tests/template-asset-contract.php
@@ -33,7 +33,7 @@ foreach ($templates as $name => $relativePath) {
 }
 
 $assetService = (string) file_get_contents($root . '/component/admin/src/Service/AdminAssetService.php');
-foreach (['addStyleSheet(', 'addScript(', '/media/com_decaromembership', '/css/admin.css'] as $marker) {
+foreach (['addStyleSheet(', 'addScript(', '/media/com_decaromembership', '/css/admin.css', '/js/admin.js'] as $marker) {
     if (!str_contains($assetService, $marker)) {
         $errors[] = "AdminAssetService missing {$marker}";
     }
